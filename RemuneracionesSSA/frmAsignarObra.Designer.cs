0d86d7b9f05cca3f3c125aaea34924be241a7883
Jorge Bascuñán <kokeshiet@gmail.com>
2018-12-02 19:10:06 -0300

    Se agrega formulario frmAsignarObra

---
+++ RemuneracionesSSA/frmAsignarObra.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace RemuneracionesSSA
+{
+    partial class frmAsignarObra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtApaterno = new System.Windows.Forms.TextBox();
+            this.txtAmaterno = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbObra = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtFechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.btnAsignar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAsignar);
+            this.groupBox1.Controls.Add(this.dtFechaInicio);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.cbObra);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtAmaterno);
+            this.groupBox1.Controls.Add(this.txtApaterno);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtDV);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtRut);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(529, 126);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Asignar Obra";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rut";
+            // 
+            // txtRut
+            // 
+            this.txtRut.Location = new System.Drawing.Point(98, 19);
+            this.txtRut.Name = "txtRut";
+            this.txtRut.Size = new System.Drawing.Size(100, 20);
+            this.txtRut.TabIndex = 1;
+            this.txtRut.TextChanged += new System.EventHandler(this.txtRut_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "-";
+            // 
+            // txtDV
+            // 
+            this.txtDV.Enabled = false;
+            this.txtDV.Location = new System.Drawing.Point(220, 19);
+            this.txtDV.Name = "txtDV";
+            this.txtDV.Size = new System.Drawing.Size(27, 20);
+            this.txtDV.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(98, 45);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(149, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Apellido Paterno";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Apellido Materno";
+            // 
+            // txtApaterno
+            // 
+            this.txtApaterno.Enabled = false;
+            this.txtApaterno.Location = new System.Drawing.Point(98, 71);
+            this.txtApaterno.Name = "txtApaterno";
+            this.txtApaterno.Size = new System.Drawing.Size(149, 20);
+            this.txtApaterno.TabIndex = 8;
+            // 
+            // txtAmaterno
+            // 
+            this.txtAmaterno.Enabled = false;
+            this.txtAmaterno.Location = new System.Drawing.Point(98, 97);
+            this.txtAmaterno.Name = "txtAmaterno";
+            this.txtAmaterno.Size = new System.Drawing.Size(149, 20);
+            this.txtAmaterno.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(295, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Obra";
+            // 
+            // cbObra
+            // 
+            this.cbObra.FormattingEnabled = true;
+            this.cbObra.Location = new System.Drawing.Point(331, 19);
+            this.cbObra.Name = "cbObra";
+            this.cbObra.Size = new System.Drawing.Size(193, 21);
+            this.cbObra.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(260, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Fecha Inicio";
+            // 
+            // dtFechaInicio
+            // 
+            this.dtFechaInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFechaInicio.Location = new System.Drawing.Point(331, 45);
+            this.dtFechaInicio.Name = "dtFechaInicio";
+            this.dtFechaInicio.Size = new System.Drawing.Size(107, 20);
+            this.dtFechaInicio.TabIndex = 13;
+            // 
+            // btnAsignar
+            // 
+            this.btnAsignar.Location = new System.Drawing.Point(331, 94);
+            this.btnAsignar.Name = "btnAsignar";
+            this.btnAsignar.Size = new System.Drawing.Size(75, 23);
+            this.btnAsignar.TabIndex = 14;
+            this.btnAsignar.Text = "Asignar";
+            this.btnAsignar.UseVisualStyleBackColor = true;
+            // 
+            // frmAsignarObra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 145);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmAsignarObra";
+            this.Text = "...::: Asignar Obra :::...";
+            this.Load += new System.EventHandler(this.frmAsignarObra_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtAmaterno;
+        private System.Windows.Forms.TextBox txtApaterno;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRut;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtFechaInicio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbObra;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnAsignar;
+    }
+}
---
+++ RemuneracionesSSA/frmAsignarObra.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+using CapaNegocio;
+
+namespace RemuneracionesSSA
+{
+    public partial class frmAsignarObra : Form
+    {
+        public frmAsignarObra()
+        {
+            InitializeComponent();
+        }
+
+        TrabajadorCE objEntidad = new TrabajadorCE();
+        TrabajadorCN objNego = new TrabajadorCN();
+
+        private void frmAsignarObra_Load(object sender, EventArgs e)
+        {
+            DataTable dtDoctores = new DataTable();
+            dtDoctores = objNego.n_listado();
+            DataRow rowDoctores;
+            rowDoctores = dtDoctores.NewRow();
+            rowDoctores[0] = "0";
+            rowDoctores[1] = "Seleccione Obra";
+            dtDoctores.Rows.InsertAt(rowDoctores, 0);
+            cbObra.DataSource = dtDoctores;
+            cbObra.DisplayMember = "nombre";
+            cbObra.ValueMember = "id_obra";
+            cbObra.SelectedItem = 0;
+            cbObra.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void txtRut_TextChanged(object sender, EventArgs e)
+        {
+            if (txtRut.Text.Length > 0)
+            {
+                txtDV.Text = CapaDatos.Util.calculaDV(Convert.ToInt32(txtRut.Text));
+                DataTable dt = objNego.n_listadoXrut(Convert.ToInt32(txtRut.Text));
+                if (dt.Rows.Count > 0)
+                {
+                    txtNombre.Text = dt.Rows[0][0].ToString();
+                    txtApaterno.Text = dt.Rows[0][1].ToString();
+                    txtAmaterno.Text = dt.Rows[0][2].ToString();
+                }
+            }
+        }
+    }
+}
