b9bbd9c7673b7d8282a7f53049e8088b87f2cff7
Jorge Bascuñán <kokeshiet@gmail.com>
2018-12-02 12:09:24 -0300

    Se agrega entidad trabajador y conexion a base de datos en app.config

---
+++ CapaEntidad/Trabajador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Trabajador
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apaterno { get; set; }
+        public string amaterno { get; set; }
+        public int rut { get; set; }
+        public string dv { get; set; }
+    }
+}
