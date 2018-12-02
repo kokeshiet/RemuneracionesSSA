8a38e4ae74cd0b64169df27629e07a35c42d5b75
Jorge Bascuñán <kokeshiet@gmail.com>
2018-12-02 12:59:28 -0300

    Se agrega clase trabajadorNE en la capa de negocio

---
+++ CapaNegocio/TrabajadorNE.cs
@@ -0,0 +1,37 @@
+﻿using capaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+namespace capaNegocios
+{
+    public class TrabajadorNE
+    {
+        TrabajadorCD objdato = new TrabajadorCD();
+
+        public DataTable n_listado()
+        {
+            return objdato.d_listado();
+        }
+
+        public void n_insertar(TrabajadorCE emp)
+        {
+            objdato.d_insertar(emp);
+        }
+
+        public void n_eliminar(int cod)
+        {
+            objdato.d_eliminar(cod);
+        }
+
+        public void n_editar(TrabajadorCE emp)
+        {
+            objdato.d_editar(emp);
+        }
+    }
+}
