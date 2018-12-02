fe73f6f7d200dc925bcfe9664971fc03506a2831
Jorge Bascuñán <kokeshiet@gmail.com>
2018-12-02 12:12:49 -0300

    Se agrega entidad obra

---
+++ CapaEntidad/Obra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Obra
+    {
+        public int id_obra { get; set; }
+        public string nombre { get; set; }
+        public string detalle { get; set; }
+    }
+}
