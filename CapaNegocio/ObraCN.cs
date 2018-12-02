ad1f7cb929514bb5865b97d2afa8228b9161bed9
Jorge Bascuñán <kokeshiet@gmail.com>
2018-12-02 12:55:58 -0300

    Se agrega clase obraCN en la capa negocio

---
+++ CapaNegocio/ObraCN.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using CapaEntidad;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class ObraCN
+    {
+        ObraCD objDato = new ObraCD();
+
+        public DataTable n_listado()
+        {
+            return objDato.d_listado();
+        }
+
+        public void n_insertar(ObraCE obr)
+        {
+            objDato.d_insertar(obr);
+        }
+
+        public void n_eliminar(int id)
+        {
+            objDato.d_eliminar(id);
+        }
+
+        public void n_editar(ObraCE obr)
+        {
+            objDato.d_editar(obr);
+        }
+    }
+}
