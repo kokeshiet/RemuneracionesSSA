dd2ef8e93ed3048427cabdbf67094b2f5db9e933
Jorge Bascuñán <kokeshiet@gmail.com>
2018-12-08 01:25:49 -0300

    Se crea entidad transacciones

---
+++ CapaEntidad/TransaccionesCE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class TransaccionesCE
+    {
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public string accion { get; set; }
+        public string detalle { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
